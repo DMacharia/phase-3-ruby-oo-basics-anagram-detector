f49e6f9d6814d673b114f9ba9f871ca53b730699
Daniel Macharia <danielhuios@gmail.com>
2022-10-22 05:01:33 +0300

    anagram completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require "pry"
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram)
+        anagram.select {|anagchars| anagchars.chars.sort == word.chars.sort}
+    end
+end
+
+listen = Anagram.new("listen")
+ba = Anagram.new("ba")
+puts ba.match(%w[ab abc back])
+puts listen.match(%w[enlists google inlets banana])
+
